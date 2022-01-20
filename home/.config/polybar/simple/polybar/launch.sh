137851675f1fe38a1b59b8e6347934e3b6f9a710
rxyhn <rayhankafipratama333@gmail.com>
2022-01-20 17:36:02 +0700

    Dashboard Updates

---
+++ home/.config/polybar/simple/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+(sleep 2; polybar -r main) &
+
+echo "Polybar launched..."
---
+++ home/.config/polybar/simple/polybar/scripts/uptime.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# literaly the simplest script.......
+
+uptime --pretty | sed 's/up //' | sed 's/\ years\?,/y/' | sed 's/\ weeks\?,/w/' | sed 's/\ days\?,/d/' | sed 's/\ hours\?,\?/h/' | sed 's/\ minutes\?/m/'
